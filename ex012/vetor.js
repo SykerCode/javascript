d8cde06a1791350455177e2e8a55ec51ee7e18d4
SykerCode <151483174+SykerCode@users.noreply.github.com>
2023-12-01 20:57:39 -0300

    Update vetor.js

--- ex012/vetor.js
+++ ex012/vetor.js
@@ -1,7 +1,7 @@
-let valores = [1, 3, 4, 5, 6, 7, 8]
-console.log(valores)
 
-for (let pos = 0; pos < valores.length
-    ; pos++) {
+
+var valores = [1, 3, 4, 5, 6, 7, 8]
+console.log(valores)
+for (var pos = 0; pos < valores.length; pos++) {
     console.log(`A posiçao ${pos} tem o valor ${valores}`)
 }
